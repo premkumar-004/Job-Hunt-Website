fcd4cb25ed2533e9f9fc335aa323e39bf6d04967
premkumar-004 <premkumarkardale@gmail.com>
2024-09-22 00:31:37 +0530

    created boilerplate for all pages templating and a footer section

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 const User = require("./models/user.js");
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, '/public')));
+app.engine("ejs", ejsMate);
+
 
 main().then(res => {
     console.log("Connection with DB successful");
@@ -16,12 +27,6 @@ async function main() {
 }
 
 
-app.set("view engine", "ejs");
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.set("views", path.join(__dirname, "/views"));
-app.use(express.static(path.join(__dirname, '/public')));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
